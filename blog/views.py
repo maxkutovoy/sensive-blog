f18fd05cbd34cf74d74fd84ff39964ff2e1c33b8
Max Kutovoy <maxkutovoy@yandex.ru>
2022-01-03 22:30:40 +0600

    Reduce queries count with prefetch_related for author

--- blog/views.py
+++ blog/views.py
@@ -34,13 +34,16 @@ def get_likes_count(post):
 
 def index(request):
     popular_posts = Post.objects.annotate(
-        likes_count=Count('likes')).order_by('-likes_count')
+        likes_count=Count('likes')).order_by('-likes_count').prefetch_related(
+        'author')
     most_popular_posts = list(popular_posts)[:5]
 
-    fresh_posts = Post.objects.order_by('published_at')
+    fresh_posts = Post.objects.order_by('published_at').prefetch_related(
+        'author')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    popular_tags = Tag.objects.annotate(tags_count=Count('posts')).order_by('-tags_count')
+    popular_tags = Tag.objects.annotate(tags_count=Count('posts')).order_by(
+        '-tags_count')
     most_popular_tags = popular_tags[:5]
 
     context = {
